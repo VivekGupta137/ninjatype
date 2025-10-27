04bb7afee8f30eca1daad1950baf46791b4a0d7a
vivek <vivekgpt107@gmail.com>
2025-10-27 15:28:30 +0530

    added config toolbar

---
+++ src/components/toolbar/ConfigMode.tsx
@@ -0,0 +1,20 @@
+import { $config, $isMounted } from "@/store/config";
+import { useStore } from "@nanostores/react";
+
+const ConfigMode = () => {
+    const config = useStore($config);
+    const handleModeChange = (mode: "time" | "words") => {
+        $config.setKey("mode", mode);
+    }
+    
+    return (<>
+        <button className="btn" data-enabled={ config.mode == "time"} onClick={() => handleModeChange("time")}>
+            <i className="material-icons">schedule</i><span>time</span>
+        </button>
+        <button className="btn" data-enabled={ config.mode == "words"} onClick={() => handleModeChange("words")}>
+            <i className="material-icons">text_format</i><span>words</span>
+        </button>
+    </>);
+}
+
+export default ConfigMode;
--- src/store/config.ts
+++ src/store/config.ts
@@ -1,8 +1,17 @@
 import type { THEME } from "@/constants/themes";
 import { persistentMap } from "@nanostores/persistent";
+import { atom } from "nanostores";
 
 export const $config = persistentMap<{
     theme: keyof typeof THEME;
+    mode: "time" | "words";
+    maxWordCount: string;
+    countdownTime: string;
 }>("config:", {
     theme: "default",
+    mode : "time", // default is time
+    maxWordCount: "25",
+    countdownTime: "15s",
 });
+
+export const $isMounted = atom<boolean>(false);
