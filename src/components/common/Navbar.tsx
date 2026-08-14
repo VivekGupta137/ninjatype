4dd42e657bb17cf46af185f6bc4577e9ef801a29
vivek gupta <vivekgpt107@gmail.com>
2026-08-14 15:21:21 +0530

    improved the ux for first page

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -10,38 +10,42 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <div className="flex gap-2 flex-wrap">
-            <Link href="/online/">
-                <Link.Icon className="size-5 mr-1">
-                    <Globe2 />
-                </Link.Icon>
-                Online
-            </Link>
-            <Link href="/">
-                <Link.Icon className="size-5 mr-1">
-                    <Keyboard />
-                </Link.Icon>
-                Practice
-            </Link>
-            <Link href="/learn/">
-                <Link.Icon className="size-5 mr-1">
-                    <GraduationCap />
-                </Link.Icon>
-                Learn
-            </Link>
-            <Link href="/history/">
-                <Link.Icon className="size-5 mr-1">
-                    <HistoryIcon />
-                </Link.Icon>
-                History
-            </Link>
-            <Link href="/settings/">
-                <Link.Icon className="size-5 mr-1">
-                    <Settings2 />
-                </Link.Icon>
-                Settings
-            </Link>
-        </div>
+        <nav className="navbar" aria-label="Site navigation">
+            <div className="navbar-primary-links">
+                <Link href="/online/">
+                    <Link.Icon className="size-5 mr-1">
+                        <Globe2 />
+                    </Link.Icon>
+                    Online
+                </Link>
+                <Link href="/">
+                    <Link.Icon className="size-5 mr-1">
+                        <Keyboard />
+                    </Link.Icon>
+                    Practice
+                </Link>
+                <Link href="/learn/">
+                    <Link.Icon className="size-5 mr-1">
+                        <GraduationCap />
+                    </Link.Icon>
+                    Learn
+                </Link>
+            </div>
+            <div className="navbar-secondary-links">
+                <Link href="/history/">
+                    <Link.Icon className="size-5 mr-1">
+                        <HistoryIcon />
+                    </Link.Icon>
+                    History
+                </Link>
+                <Link href="/settings/">
+                    <Link.Icon className="size-5 mr-1">
+                        <Settings2 />
+                    </Link.Icon>
+                    Settings
+                </Link>
+            </div>
+        </nav>
     );
 };
 
